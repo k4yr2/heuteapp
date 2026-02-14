2a000aa19e8313b6faaf4ccdb2b411fbcc267f5d
k4yr2 <kyrzkaya@gmail.com>
2026-02-14 04:38:00 +0300

    feat: add SidebarItem component to HeuteSidebar

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -3,7 +3,17 @@ import styles from './sidebar.module.css';
 export default function HeuteSidebar() {
   return (
     <div id="heute-sidebar" className={styles.sidebar}>
-
+        <SidebarItem />
     </div>
   );
+}
+
+//
+
+const SidebarItem : React.FC = () => {
+    return (
+        <div className={styles.item}>
+            A
+        </div>
+    );
 }
