60b47bdf2dea79a638aeaf841e234a3903994865
k4yr2 <kyrzkaya@gmail.com>
2026-02-18 02:39:55 +0300

    fix: update grid sizes in default layout of HeuteDayboard

--- src/heute/dayboard.ts
+++ src/heute/dayboard.ts
@@ -11,11 +11,11 @@ export class HeuteDayboard {
             createLayout("default", {
                 grids: [
                     createGrid("first", {
-                        size: { cols: 12, rows: 6 },
+                        size: { cols: 18, rows: 4 },
                         bounds: { x1: 0, y1: 0, x2: 100, y2: 50 },
                     }),
                     createGrid("second", {
-                        size: { cols: 12, rows: 6 },
+                        size: { cols: 18, rows: 4 },
                         bounds: { x1: 0, y1: 50, x2: 100, y2: 100 },
                     }),
                 ],
