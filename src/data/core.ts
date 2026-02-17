ba54b58498fbc96be9320c1001aadcffd5f3bba4
k4yr2 <kyrzkaya@gmail.com>
2026-02-17 17:55:58 +0300

    refactor: remove AbsolutePosition and RelativePosition interfaces, streamline Point interface

--- src/data/core.ts
+++ src/data/core.ts
@@ -1,13 +1,14 @@
-export interface AbsolutePosition {
-    x1: number;
-    y1: number;
-    x2: number;
-    y2: number;
-}
-
-export interface RelativePosition {
+export interface Point {
     x: number;
     y: number;
+}
+
+export interface Rect extends Point {
     width: number;
     height: number;
+}
+
+export interface RectBounds extends Point {
+    x2: number;
+    y2: number;
 }
