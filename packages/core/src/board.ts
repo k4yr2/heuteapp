5905c7e4bbf2dde99e68338d9cc9eb796f10ae29
k4yr2 <kyrzkaya@gmail.com>
2026-02-21 00:58:03 +0300

    refactor: remove HeuteBoardLayouts class and related layout management from HeuteBoard

--- packages/core/src/board.ts
+++ packages/core/src/board.ts
@@ -1,32 +1,5 @@
-import { boardLayout, boardField } from "@/src/library/models";
-import HeuteBoardLayouts from "../boardLayouts";
 
 export class HeuteBoard {
-    #layouts : HeuteBoardLayouts;
-
-    constructor() {
-        this.#layouts = new HeuteBoardLayouts();
-
-        this.#layouts.add(
-            boardLayout("default", {
-                fields: [
-                    boardField("first", {
-                        bounds: { x1: 0, y1: 0, x2: 100, y2: 50 },
-                        grid: { cols: 18, rows: 4 }
-
-                    }),
-                    boardField("second", {
-                        bounds: { x1: 0, y1: 50, x2: 100, y2: 100 },
-                        grid: { cols: 18, rows: 4 }
-                    }),
-                ],
-            })
-        );
-    }
-
-    public get layouts(): HeuteBoardLayouts {
-        return this.#layouts;
-    }
 }
 
 export default HeuteBoard;
