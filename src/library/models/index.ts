44d0deed924a3b5fc79753a1bc759b939e83dd50
k4yr2 <kyrzkaya@gmail.com>
2026-02-20 19:57:58 +0300

    feat: add initial export of models and components in index.ts

---
+++ src/library/models/index.ts
@@ -0,0 +1,7 @@
+export type { BoardModel } from "./board";
+export type { BoardFieldModel } from "./boardField";
+export type { BoardGridModel } from "./boardGrid";
+export type { BoardLayoutModel } from "./boardLayout";
+
+export { boardField } from "./boardField";
+export { boardLayout } from "./boardLayout";
