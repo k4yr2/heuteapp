4b0ed40108390a393d858f05f1df54a397ddc74c
k4yr2 <kyrzkaya@gmail.com>
2026-02-22 04:44:51 +0300

    feat: implement properties and constructor for HeuteBoardGrid class

--- libs/core/src/board-grid.ts
+++ libs/core/src/board-grid.ts
@@ -1,5 +1,19 @@
 export class HeuteBoardGrid {
-    
+    #cols: number;
+    #rows: number;
+
+    constructor(cols: number, rows: number) {
+        this.#cols = cols;
+        this.#rows = rows;
+    }
+
+    public get cols() {
+        return this.#cols;
+    }
+
+    public get rows() {
+        return this.#rows;
+    }
 }
 
 export default HeuteBoardGrid;
