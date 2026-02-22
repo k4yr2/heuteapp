4518b9b1377335d32d0ae6f309bec3649ce46f41
k4yr2 <kyrzkaya@gmail.com>
2026-02-22 08:48:58 +0300

    feat: extend HeuteBoardCard constructor to include position and content management

--- libs/core/src/board-card.ts
+++ libs/core/src/board-card.ts
@@ -1,14 +1,30 @@
+import { GridRect } from "@heuteapp/common";
 import { HeuteBoardSection } from "./board-section";
 
 export class HeuteBoardCard {
-    #section: HeuteBoardSection;
+    #section : HeuteBoardSection;
+    #position : GridRect;
+    #content : string | undefined;
 
-    constructor(section: HeuteBoardSection) {
+    constructor(section: HeuteBoardSection, position: GridRect) {
         this.#section = section;
+        this.#position = position;
     }
 
     public get section(): HeuteBoardSection {
         return this.#section;
+    }
+
+    public get position(): GridRect {
+        return this.#position;
+    }
+
+    public get content(): string | undefined {
+        return this.#content;
+    }
+
+    public setContent(content: string) {
+        this.#content = content;
     }
 }
 
