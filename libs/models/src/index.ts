5fea43df4df8b9faf3275ddee558e64d8de57eda
k4yr2 <kyrzkaya@gmail.com>
2026-02-22 07:32:43 +0300

    feat: add missing export for board in index.ts

--- libs/models/src/index.ts
+++ libs/models/src/index.ts
@@ -3,6 +3,7 @@ export type { BoardLayoutModel } from "./board-layout";
 export type { BoardSectionModel } from "./board-section";
 export type { BoardCardModel } from "./board-card";
 
+export { board } from "./board";
 export { boardLayout } from "./board-layout";
 export { boardSection } from "./board-section";
 export { boardCard } from "./board-card";
