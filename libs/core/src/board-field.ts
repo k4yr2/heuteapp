c094251fe5c0af49df302e50dcfbbf4f1afb04b7
k4yr2 <kyrzkaya@gmail.com>
2026-02-22 04:46:49 +0300

    feat: implement HeuteBoardField class with layout property and constructor

--- libs/core/src/board-field.ts
+++ libs/core/src/board-field.ts
@@ -1,4 +1,15 @@
+import HeuteBoardLayout from "./board-layout";
+
 export class HeuteBoardField {
+    #layout: HeuteBoardLayout;
+
+    constructor(layout: HeuteBoardLayout) {
+        this.#layout = layout;
+    }
+
+    public get layout() {
+        return this.#layout;
+    }
 }
 
 export default HeuteBoardField;
